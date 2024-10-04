607212ee9fc9b4c78920ab8d902dfe3b4eba5b37
Anthonius Munthi <me@itstoni.com>
2024-10-04 12:30:38 +0800

    feat(api): added cloudinary for production environment :rocket:

--- apps/api/config/plugins.ts
+++ apps/api/config/plugins.ts
@@ -1,1 +1,23 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: (() => {
+      if ("development" == process.env.NODE_ENV) {
+        return {};
+      }
+
+      return {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env("CLOUDINARY_NAME"),
+          api_key: env("CLOUDINARY_KEY"),
+          api_secret: env("CLOUDINARY_SECRET"),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      };
+    })(),
+  },
+});
